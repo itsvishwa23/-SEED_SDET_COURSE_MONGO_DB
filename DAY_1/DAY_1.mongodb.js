3821d0d31fb453831334e37cdbfc30d8f2d28c05
itsvishwa23 <itsvishwa23@gmail.com>
2024-04-10 11:10:23 +0530

    Day 1

---
+++ DAY_1/DAY_1.mongodb.js
@@ -0,0 +1,23 @@
+show dbs;
+use database (student);
+
+db.createCollection("students")
+show dbs;
+
+db.students.insertOne
+({
+        name:"Vishwanath"
+})
+
+
+db.students.insertOne
+({
+        name:"Vishwanath Taware"
+})
+
+
+db.students.find()
+
+
+
+use database (demo);
